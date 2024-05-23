3a0d45b4cc0f32b34682d426e5c9fd67ef230b3e
Priyadharshini <priya@Priyadharshini>
2024-05-23 13:57:50 +0530

    app.js modified

--- src/App.js
+++ src/App.js
@@ -8,20 +8,17 @@ import Book from './Book';
 import axios from 'axios';
 
 axios.defaults.baseURL = "https://d455-16-16-120-213.ngrok-free.app";
-axios.defaults.headers = {
-  "Content-Type":"application.json",
-  "ngrok-skip-browser-warning":"69420",
-};
+axios.defaults.headers.common["Content-Type"] = "application/json";  // Fixed the typo here
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path='/' element = {<Login/>}/>
-      <Route path='/LoginForm' element={<LoginForm/>}/>
-      <Route path='/Signup' element={<Signup/>}/>
-      <Route path='/Home' element={<Home/>}/>
-      <Route path='/Book' element={<Book/>}/>
+      <Route path='/' element={<Login />} />
+      <Route path='/LoginForm' element={<LoginForm />} />
+      <Route path='/Signup' element={<Signup />} />
+      <Route path='/Home' element={<Home />} />
+      <Route path='/Book' element={<Book />} />
     </Routes>
     </BrowserRouter>
   );
